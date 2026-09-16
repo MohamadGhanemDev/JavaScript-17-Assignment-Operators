d88f3147e21a1a1ebdd1d81131f6347618dd976e
MohamadGhanemDev <mohamad.d.ghanem@gmail.com>
2026-09-16 16:02:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// assign 10 to variable a
+let a= 10;
+
+a=20; // update a to 20
+
+a += 10;// add 10 to a ; in meaning old value + new value / another way a = a + 20
+
+a += 70 ; // output 100
+
+a -= 50; 
+
+a *= 2;
+
+a /= 2;
+console.log(a);
